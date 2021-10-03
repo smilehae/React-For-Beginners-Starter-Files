c46d41df92a119e163d8c5d5c44e01b3dba9237f
smilehae <mihaepyun@gmail.com>
2021-10-04 00:05:13 +0900

    #8 완료. render만 하는 클래스의 경우 stateless function으로 만들어 html을 리턴시켜도 좋다.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component{
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header tagline="seafood market" age={500} cool={true}/>
+                    <Header tagline="seafood market" age={100}/>
                 </div>
                 <Order/>
                 <Inventory/>
--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,24 +1,43 @@
 import React from 'react';
 
-class Header extends React.Component{
-    render(){
-        return (
-            <header className="top">
-                <h1>
-                    Catch
-                    <span className="ofThe">
-                        <span className="of">Of</span>
-                        <span className="the">the</span>
-                    </span>
-                    Day
-                </h1>
-                <h3 className="tagline">
-                    <span>{this.props.tagline}</span>
-                </h3>
-            </header>
+
+const Header=(props)=>(
+    <header className="top">
+        <h1>
+            Catch
+            <span className="ofThe">
+                <span className="of">Of</span>
+                <span className="the">the</span>
+            </span>
+            Day
+        </h1>
+        <h3 className="tagline">
+            <span>{props.tagline} {props.age}</span>
+        </h3>
+    </header>
+);
+
+//이 클래스는 사실 데이터를 받아 render하는 것 말고는 역할이 없었다. 
+//따라서 클래스로 만들기보다는 stateless function으로 구현해서, props를 매개변수로 받아서 표현하는 html자체를 리턴하게 구현해도 된다.
+// class Header extends React.Component{
+//     render(){
+//         return (
+//             <header className="top">
+//                 <h1>
+//                     Catch
+//                     <span className="ofThe">
+//                         <span className="of">Of</span>
+//                         <span className="the">the</span>
+//                     </span>
+//                     Day
+//                 </h1>
+//                 <h3 className="tagline">
+//                     <span>{this.props.tagline}</span>
+//                 </h3>
+//             </header>
             
-        )
-    }
-}
+//         )
+//     }
+// }
 
 export default Header;
